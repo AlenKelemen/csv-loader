05a4b1b6883cba39736922f2a07faaf847b60f92
Sebastijan Kelemen <skelemen@racunarstvo.hr>
2021-04-12 03:08:21 +0200

    Release 1.0, fix bugs

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
 	"encoding/csv"
 	"flag"
-	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+const FLOW_FILE_LEN = 6
+const FLOW_TYPE_INVALID = 0
+const DATE_FORMAT = "2006-01-02 15:04:05.000"
+const DATABASE_ENV = "TH_DATABASE_PATH"
+const DATABASE_DRIVER = "pgx"
+
+type FlowData struct {
+	dateTaken       time.Time
+	rawValue        int
+	MeasurementType int
+}
+
 func main() {
 	dir := loadFlag()
 	initWatcher(dir)
-
 }
 
 func loadFlag() *string {
@@ -42,7 +53,7 @@ func initWatcher(dir *string) {
 					return
 				}
 				if event.Op&fsnotify.Write == fsnotify.Write {
-					parseCSV(event.Name)
+					readCSV(event.Name)
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -60,57 +71,81 @@ func initWatcher(dir *string) {
 	<-done
 }
 
-func parseCSV(path string) {
-	file, err := os.Open(path)
+func readCSV(path string) {
+	file, err := os.ReadFile(path)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer file.Close()
+	data := string(file)
 
-	reader := csv.NewReader(bufio.NewReader(file))
+	reader := csv.NewReader(strings.NewReader(data))
+	reader.FieldsPerRecord = -1
 
-	reader.Read()
+	result, err := reader.ReadAll()
 
-	for {
-		row, err := reader.Read()
-
-		if err == io.EOF {
-			break
-		}
-
-		writeToDatabase(row)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	parseCSV(result)
 }
 
-func writeToDatabase(row []string) {
-
-	if len(row) != 4 {
+func parseCSV(data [][]string) {
+	// Dirty hack
+	if len(data) != FLOW_FILE_LEN {
 		return
 	}
 
-	db, err := sql.Open("pgx", os.Getenv("TH_DATABASE_PATH"))
+	location := time.Now().Local().Location()
+	date, err := time.ParseInLocation(DATE_FORMAT, data[2][1], location)
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	flowOne, err := strconv.Atoi(data[3][2] + data[2][2])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flowTwo, err := strconv.Atoi(data[5][2] + data[4][2])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fA := FlowData{MeasurementType: 10, dateTaken: date, rawValue: flowOne}
+	writeToDatabase(fA)
+	fB := FlowData{MeasurementType: 11, dateTaken: date, rawValue: flowTwo}
+	writeToDatabase(fB)
+}
+
+func writeToDatabase(flowData FlowData) {
+	if flowData.MeasurementType == FLOW_TYPE_INVALID {
+		return
+	}
+
+	db, err := sql.Open(DATABASE_DRIVER, os.Getenv(DATABASE_ENV))
+
+	if err != nil {
+		log.Println(err)
 	}
 
 	defer db.Close()
 
-	format := "2006-01-02 15:04:05"
-	date, err := time.Parse(format, row[1])
-
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
-	sqlStatement := `INSERT INTO main.reading (device_id, date_taken, raw_value , category_id)
+	sql := `INSERT INTO main.reading (device_id, date_taken, raw_value , category_id)
 				     VALUES ($1, $2, $3, $4)`
 
-	_, err = db.Exec(sqlStatement, 177, date, row[2], 1)
+	_, err = db.Exec(sql, 177, flowData.dateTaken, flowData.rawValue, flowData.MeasurementType)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 }
